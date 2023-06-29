6a0c4e7ea026d8f3acef82accf32934cbc25b9d6
angalland <angalland@laposte.net>
2023-06-29 16:56:25 +0200

    navbar template

--- Controller/ForetController.php
+++ Controller/ForetController.php
@@ -7,6 +7,13 @@ class ForetController {
     
     // affiche la list des foret
     public function listForet() {
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->prepare("
+            SELECT *
+            FROM foret
+        ");
+        $requete->execute();
+        
         require "view/foret/listForet.php";
     }
 }
--- View/template.php
+++ View/template.php
@@ -12,4 +12,29 @@
         </head>
 
         <body>
+            <nav class="navbar">
+                <a href="index.php?action=accueille" class="logo"><img src="public/logo/logoForet.jpg"/>
+                <div class="nav-link">
+                    <ul>
+                        <li><a href="index.php?action=listForet">Forêt</a></li>
+                        <li><a href="index.php?action=listArbre">Arbres</a></li>
+                        <li><a href="index.php?action=listPlante">Plantes</a></li>
+                        <li><a href="index.php?action=listAnimaux">Animaux</a></li>
+                        <li><a href="index.php?action=listRandonnee">Randonnées</a></li>
+                        <?php
+                        if (isset($_SESSION['user'])){?>
+                            <li><a href="index.php?action=deconnexion">Déconnexion</a></li>
+                            <li><a href="index.php?action=utilisateur">Mon Compte</a></li>
+                            <?php 
+                        } else {?>
+                            <li><a href="index.php?action=connexion">Connexion</a></li>
+                            <?php
+                        }?>
+                    </ul>
+                </div>
+            </nav>
+
             <header></header>
+            <div><?= $contenu ?></div>
+        </body>
+    </html>
