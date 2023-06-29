1ab0fb0d70b208706bdae718cf4566accb973c90
angalland <angalland@laposte.net>
2023-06-29 14:30:40 +0200

    code page index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+use Controller\ForetController;
+
+spl_autoload_register(function ($class_name){
+    include $class_name . '.php';
+});
+
+$ctrlForet = new ForetController();
+
+if(isset($_GET['action'])){
+    switch ($_GET['action']) {
+
+        // controller utilisateur
+        case 'acceuille' : $ctrlForet->viewAccueille();
+        break;
+
+        // crontoller foret
+        case 'listForet' : $ctrlForet->listForet();
+        break;
+
+        case 'detailForet' : $ctrlForet->detailFilm();
+        break;
+
+    }
+}
