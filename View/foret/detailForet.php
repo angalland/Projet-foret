7a63c5d085ae497760570609d97942468fe318c0
angalland <angalland@laposte.net>
2023-06-30 14:13:51 +0200

    methode controller detailForet + view detailForet

--- View/foret/detailForet.php
+++ View/foret/detailForet.php
@@ -1,16 +1,18 @@
 <?php
 ob_start();?>
+<div id="divDetailForet">
+    <article class="articleDetailForet">
+        <?php foreach ($requete as $foret){?>
+            <h1><?=$foret['nom_foret']?></h1>
+            <p><?=$foret['descriptif']?></p>
+            <img class="imgDetailForet" src="<?=$foret['photo']?>"/><?php
+        }?>
+    </article>
 
-<article class="articleDetailForet">
-    <?php foreach ($requete as $foret){?>
-        <h1><?=$foret['nom_foret']?></h1>
-        <p><?=$foret['descriptif']?></p>
-        <img class="imgDetailForet" src="<?=$foret['photo']?>"/><?php
-    }?>
-</article>
-
-<!-- carte randonnee -->
-<div id="map"></div>
+    <!-- carte randonnee -->
+    <h2 id="h2detailForet">La randonnée incontournable</h2>
+    <div id="map"></div>
+</div>
 
 <?php
 $titre = 'Detail forêt';
