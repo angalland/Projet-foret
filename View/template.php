295ee8fd02d0d71aa5772f021a9fc3f507ade43c
angalland <angalland@laposte.net>
2023-06-30 11:56:21 +0200

    footer

--- View/template.php
+++ View/template.php
@@ -18,39 +18,47 @@
         </head>
 
         <body>
-            <nav class="navbar">
-                <a href="index.php?action=accueille" class="logo"><img src="public/logo/logoForet.jpg"/>
-                <div class="nav-link">
-                    <ul>
-                        <li><a href="index.php?action=listForet">Forêt</a></li>
-                        <li><a href="index.php?action=listArbre">Arbres</a></li>
-                        <li><a href="index.php?action=listPlante">Plantes</a></li>
-                        <li><a href="index.php?action=listAnimaux">Animaux</a></li>
-                        <li><a href="index.php?action=listRandonnee">Randonnées</a></li>
-                        <?php
-                        if (isset($_SESSION['user'])){?>
-                            <li><a href="index.php?action=deconnexion">Déconnexion</a></li>
-                            <li><a href="index.php?action=utilisateur">Mon Compte</a></li>
-                            <?php 
-                        } else {?>
-                            <li><a href="index.php?action=connexion"><img class="logoConnexion" src="public/logo/user-regular.svg" /></a></li>
+            <div class="contenu">
+                <nav class="navbar">
+                    <a href="index.php?action=accueille" class="logo"><img src="public/logo/logoForet.jpg"/>
+                    <div class="nav-link">
+                        <ul>
+                            <li><a href="index.php?action=listForet">Forêt</a></li>
+                            <li><a href="index.php?action=listArbre">Arbres</a></li>
+                            <li><a href="index.php?action=listPlante">Plantes</a></li>
+                            <li><a href="index.php?action=listAnimaux">Animaux</a></li>
+                            <li><a href="index.php?action=listRandonnee">Randonnées</a></li>
                             <?php
-                        }?>
-                    </ul>
-                </div>
-            </nav>
+                            if (isset($_SESSION['user'])){?>
+                                <li><a href="index.php?action=deconnexion">Déconnexion</a></li>
+                                <li><a href="index.php?action=utilisateur">Mon Compte</a></li>
+                                <?php 
+                            } else {?>
+                                <li><a href="index.php?action=connexion"><img class="logoConnexion" src="public/logo/user-regular.svg" /></a></li>
+                                <?php
+                            }?>
+                        </ul>
+                    </div>
+                </nav>
 
-            <header>
-                    <form id="formRecherche" action="#" method="POST">
-                        <button id="barreRecherche" type="text" name="recherche">
-                            <img id="loupe_recherche" src="public/logo/loupe_recherche.svg" />
-                            Recherche
-                        </button>
-                    </form>
-            </header>
-            <main><?= $contenu ?></main>
+                <header>
+                        <form id="formRecherche" action="#" method="POST">
+                            <button id="barreRecherche" type="text" name="recherche">
+                                <img id="loupe_recherche" src="public/logo/loupe_recherche.svg" />
+                                Recherche
+                            </button>
+                        </form>
+                </header>
+                <main><?= $contenu ?></main>
+            </div>
             <footer>
-                
+                    <ul class="list-services">
+                        <li><a href="#">© 2023 - Forêst compagnie</a></li>
+                        <li><a href="#">Action de Forêst</a></li>
+                        <li><a href="#">Nous contacter</a></li>
+                        <li><a href="#">Mention légale</a></li>
+                        <li><a href="#">Vos informations personnelles</a></li>
+                    <ul>                           
             </footer>
             
             <script src="public/javascript/leafetJs.js"></script>
