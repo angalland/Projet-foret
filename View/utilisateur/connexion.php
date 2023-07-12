9c3bfe03d84f61fb892ef1c029a7636108677158
angalland <angalland@laposte.net>
2023-07-12 13:09:58 +0200

    ajout formulaire connexion

--- View/utilisateur/connexion.php
+++ View/utilisateur/connexion.php
@@ -1,11 +1,24 @@
 <?php
 ob_start();?>
 
-<form 
+<form id="formConnexion" method="POST" action="index.php?action=login">
+    <h3>Connexion</h3>
+    <div>
+        <input type="text" placeholder="Pseudo" name="pseudo">
+    </div>
+    <div>
+        <input type="email" placeholder="email" name="email">
+    </div>
+    <div>
+        <input type="password" placeholder="Mot de passe" name="password">
+    </div>
+    <div>
+        <input type="submit" value='connexion'>
+    </div>
+</form>
 
 
-
-
+<?php
 $titre = 'Connexion';
 $contenu = ob_get_clean();
 require "view/template.php";
