195dacf4ae4fe2de0892b7a323e3625533df09b0
angalland <angalland@laposte.net>
2023-07-24 12:55:29 +0200

    css form addForet

--- View/foret/ajouterForet.php
+++ View/foret/ajouterForet.php
@@ -1,24 +1,38 @@
 <?php
 ob_start();?>
 
-<h2>Ajouter une forêt</h2>
 
-<form action="index.php?action=addForet" method="POST" enctype="multipart/form-data">
+<form id="formConnexion" action="index.php?action=addForet" method="POST" enctype="multipart/form-data">
+    <h3 class="formH3">Ajouter une forêt</h3>
 
-    <label for="nom_foret">Nom</label>
-    <input type="text" name="nom_foret" id="nom_foret">
+    <div>
+        <label for="nom_foret">Nom</label>
+        <input type="text" name="nom_foret" id="nom_foret" class="inputConnexion">
+    </div>
 
-    <label for="ville">Ville</label>
-    <input type="text" name="ville" id="ville">
+    <div>
+        <label for="ville">Ville</label>
+        <input type="text" name="ville" id="ville" class="inputConnexion">
+    </div>
 
-    <label for="code_postal">Code postal</label>
-    <input type="text" name="code_postal" id="code_postal">
+    <div>
+        <label for="code_postal">Code postal</label>
+        <input type="text" name="code_postal" id="code_postal" class="inputConnexion">
+    </div>
 
-    <label for="photo">Photo</label>
-    <input type="file" name="photo" id="photo">
+    <div>
+        <label for="photo">Photo</label>
+        <input type="file" name="photo" id="photo" class="inputConnexion">
+    </div>
 
-    <label for="descriptif">Description</label>
-    <input type="text" name="descriptif" id="descriptif">
+    <div>
+        <label for="descriptif">Description</label>
+        <input type="text" name="descriptif" id="descriptif" class="inputConnexion">
+    </div>
+
+    <div>
+        <input class="button" type="submit" value='Ajouter'>
+    </div>
 </form>
 
 <?php
