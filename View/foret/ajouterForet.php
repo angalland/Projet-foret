4c9891490f90ede48dc2aba2c0c3bf5ec1a82db3
angalland <angalland@laposte.net>
2023-07-24 09:17:57 +0200

    creation du formulaire addforet

--- View/foret/ajouterForet.php
+++ View/foret/ajouterForet.php
@@ -1,6 +1,26 @@
 <?php
 ob_start();?>
 
+<h2>Ajouter une forêt</h2>
+
+<form action="index.php?action=addForet" method="POST" enctype="multipart/form-data">
+
+    <label for="nom_foret">Nom</label>
+    <input type="text" name="nom_foret" id="nom_foret">
+
+    <label for="ville">Ville</label>
+    <input type="text" name="ville" id="ville">
+
+    <label for="code_postal">Code postal</label>
+    <input type="text" name="code_postal" id="code_postal">
+
+    <label for="photo">Photo</label>
+    <input type="file" name="photo" id="photo">
+
+    <label for="descriptif">Description</label>
+    <input type="text" name="descriptif" id="descriptif">
+</form>
+
 <?php
 $titre = 'Ajouter une forêt';
 $contenu = ob_get_clean();
