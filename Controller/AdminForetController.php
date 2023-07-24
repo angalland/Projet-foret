cccad12065bf83e18be07a52c798e4625dbc83ff
angalland <angalland@laposte.net>
2023-07-24 13:42:20 +0200

    methode addForet filtre + addd photo

--- Controller/AdminForetController.php
+++ Controller/AdminForetController.php
@@ -15,9 +15,60 @@ class AdminForetController {
         // verifie qu'on a bien appuyer sur ajouter
         if (isset($_POST['submitAddForet'])){
             
+            // créer un tableau de $_SESSION["errors"] qui servira a traiter tous les erreures
+            $_SESSION["errors"] = [];
+
             // filtre les données envoyées
             $nom_foret = htmlspecialchars($_POST['nom_foret']);
+            $ville = htmlspecialchars($_POST['ville']);
+            $code_postal = htmlspecialchars($_POST['code_postal']);
+            $descriptif = htmlspecialchars($_POST['descriptif']);
+
+            // filtre et dowload le fichier photo
+            if (isset($_FILES["photo"])){ // vérifie que l'utilisateur a bien transmis son fichier 
+
+                // Récupere les informations du fichiers
+                $tmpName = $_FILES["photo"]['tmp_name']; 
+                $nameImage = $_FILES["photo"]["name"];
+                $type = $_FILES["photo"]["type"];
+                $error = $_FILES["photo"]["error"];
+                $size = $_FILES["photo"]["size"];
             
+                // separe la chaine de caractere $name a chaque fois qu'il a un "."
+                $tabExtension = explode('.', $nameImage);               
+                // Prend le dernier element de $tabExtension et le renvoie en minuscule
+                $extension = strtolower(end($tabExtension));                
+                // Introduit une variable ayant pour valeur un int
+                $tailleMax = 3000000;                
+                //Tableau des extensions qu'on autorise 
+                $extensionAutorisees = ['jpg', 'jpeg', 'gif', 'png'];
+
+                    if(in_array($extension, $extensionAutorisees) && $size <= $tailleMax && $error == 0 ){ // vérifie que $extension soit compris dans $extensionAutorisees et que la taille du fichier soit <= a la valeur de $tailleMax et que le fichier ne renvoie aucune erreure
+
+                        // génere un nom unique ex: 5f586bf96dcd38.73540086
+                        $uniqueName = uniqid('', true);
+
+                        // on ajoute $uniqueName avec $extension = 5f586bf96dcd38.73540086.jpg
+                        $fileName = $uniqueName.'.'.$extension;
+                        
+                        //transfere le fichier img ($tmpName etant le chemin ou il est sur l'ordinateur dans le fichier /upload/ et lui assigne $fileName)
+                        move_uploaded_file($tmpName, 'public/img/forêt'.$fileName);
+
+                    } elseif (in_array($extension, $extensionAutorisees) == false) { // sinon
+                            
+                        // envoie un message d'alerte si la premiere condition n'est pas respecté et le fichier ne sera pas transmis
+                        $_SESSION['errors'] [] = "Le fichier n'a pas été ajouté, vous devez transmettre des fichiers au format jpg, jpeg, gif ou png";
+                        
+                    } elseif ($size > $tailleMax) { // sinon 
+                        
+                        // envoie un message d'alerte si la taille du fichier dépasse la taille autorisé
+                        $_SESSION['errors'] [] = "Le fichier n'a pas été ajouté, vous devez transmettre des fichiers de moins de 3 méga";
+                    }
+
+                    if (isset($fileName)) {
+                    $affiche = "public/img/forêt".$fileName; // crée une variable qui = au chemin d'acces du fichier dans le dossier upload
+                    }
+            }        
         }   
 
     }
