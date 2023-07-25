6c9656f034020a6000593da5e92ed5079cb9ebf5
angalland <angalland@laposte.net>
2023-07-25 09:23:36 +0200

    form update foret

--- View/foret/modifierForet.php
+++ View/foret/modifierForet.php
@@ -1,32 +1,21 @@
 <?php
 ob_start();
 
-foreach ($requete as $foret) {
-    $id_forets [] = $foret['id_foret'];
-    $nom_forets [] = $foret['nom_foret'];
-    $ville = $foret['ville'];
-    $code_postal = $foret['code_postal'];
-    $photo = $foret['photo'];
-    $descriptif = $foret['descriptif'];
-}
-var_dump($id_forets);
-var_dump($nom_forets);
+
 ?>
-<form id="formAddForet" class="formH3 addH3Foret" action="" method="POST">
+<form id="formAddForet" class="formH3 addH3Foret" action="" method="GET">
     <h3 class="formH3 addH3Foret">Choississez la forêt à modifier</h3>
 
     <div class="addDivForet">
         <label class="addLabel" for="nom_foret">Forêt : </label>
         <select name="forêt" id="nom_foret" class="inputConnexion">
             <option value=""></option>
-            <option
-            <?php foreach ($id_forets as $id_foret){?>
-                    value="<?=$id_foret?>"
-                    <?php } ?>><?php
-                    foreach ($nom_forets as $nom_foret){?>
-                        <?=$nom_foret?><?php
-                    }?>
-            </option>                      
+            <?php
+            foreach ($requete as $foret){?>
+                <option value="<?=$foret["id_foret"]?>">
+                <?=$foret['nom_foret']?></option>                      
+            <?php
+            }?>
         </select>
     </div>
 
