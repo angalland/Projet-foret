fac2a958d8d4ae4d34cf04433335c6463890823f
angalland <angalland@laposte.net>
2023-07-26 08:22:40 +0200

    form update classe

--- View/taxonomie/modifierSupprimerTaxonomie.php
+++ View/taxonomie/modifierSupprimerTaxonomie.php
@@ -1,6 +1,27 @@
 <?php
 ob_start();?>
 
+<form id="formAddForet" class="formH3 addH3Foret" action="index.php?action=viewUpdateForetById" method="POST">
+    <h3 class="formH3 addH3Foret">Choississez la classe à modifier ou Supprimer</h3>
+
+    <div class="addDivForet">
+        <label class="addLabel" for="nom_foret">Classe : </label>
+        <select name="foret" id="nom_foret" class="inputConnexion">
+            <option value=""></option>
+            <?php
+            foreach ($requeteClasse as $classe){?>
+                <option value="<?=$classe["id_classe"]?>">
+                <?=$classe['nom_classe']?></option>                      
+            <?php
+            }?>
+        </select>
+    </div>
+
+    <div class="addButtonForet ">
+        <input class="button updateButtonForet" type="submit" name="submitUpdateForet" value='Modifier'>
+    </div>
+</form>
+
 <?php
 // affiche un message succes si il y en a un 
 if (isset($_SESSION['messageSucces'])) {?>
