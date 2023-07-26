fd33e9644391d7d511e2ba015bfed5418b251863
angalland <angalland@laposte.net>
2023-07-26 08:25:30 +0200

    css from update taxonomie

--- View/taxonomie/modifierSupprimerTaxonomie.php
+++ View/taxonomie/modifierSupprimerTaxonomie.php
@@ -1,26 +1,27 @@
 <?php
 ob_start();?>
+<div class="taxonomie">
+    <form class="formClasse" action="index.php?action=viewUpdateForetById" method="POST">
+        <h3 class="formH3 addH3Foret">Choississez la classe à modifier ou Supprimer</h3>
 
-<form id="formAddForet" class="formH3 addH3Foret" action="index.php?action=viewUpdateForetById" method="POST">
-    <h3 class="formH3 addH3Foret">Choississez la classe à modifier ou Supprimer</h3>
+        <div class="divClasse">
+            <label class="addLabel" for="nom_foret">Classe : </label>
+            <select name="foret" id="nom_foret" class="inputConnexion">
+                <option value=""></option>
+                <?php
+                foreach ($requeteClasse as $classe){?>
+                    <option value="<?=$classe["id_classe"]?>">
+                    <?=$classe['nom_classe']?></option>                      
+                <?php
+                }?>
+            </select>
+        </div>
 
-    <div class="addDivForet">
-        <label class="addLabel" for="nom_foret">Classe : </label>
-        <select name="foret" id="nom_foret" class="inputConnexion">
-            <option value=""></option>
-            <?php
-            foreach ($requeteClasse as $classe){?>
-                <option value="<?=$classe["id_classe"]?>">
-                <?=$classe['nom_classe']?></option>                      
-            <?php
-            }?>
-        </select>
-    </div>
-
-    <div class="addButtonForet ">
-        <input class="button updateButtonForet" type="submit" name="submitUpdateForet" value='Modifier'>
-    </div>
-</form>
+        <div class="addButtonForet ">
+            <input class="button" type="submit" name="submitUpdateForet" value='Modifier'>
+        </div>
+    </form>
+<div>
 
 <?php
 // affiche un message succes si il y en a un 
