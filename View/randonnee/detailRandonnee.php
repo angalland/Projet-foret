f202ce2d8b744f3c48e253e4242e7f72269916f9
angalland <angalland@laposte.net>
2023-07-28 11:30:24 +0200

    leaflet detailRandonnee

--- View/randonnee/detailRandonnee.php
+++ View/randonnee/detailRandonnee.php
@@ -1,7 +1,28 @@
 <?php
 ob_start();?>
-
 <?php
+foreach ($requeteRandonnee as $randonnee){?>
+                    <div id="<?=$randonnee['id_randonnee']?>" style="width:400px;height:400px;margin:30px"></div>
+                    <script>
+                        // initialisation de la carte leaflet et du zoom
+                        var map = L.map('<?=$randonnee['id_randonnee']?>').setView([<?=$randonnee['longitude']?>, <?=$randonnee['lattitude']?>], 14);
+                        // gestion des tuiles
+                        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                        maxZoom: 19,
+                        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                        }).addTo(map);
+                        // marker sur la carte
+                        var marker = L.marker([<?=$randonnee['longitude']?>, <?=$randonnee['lattitude']?>]).addTo(map);
+                        // popup sur le marker
+                        marker.bindPopup("<?=$randonnee['nom_randonnee']?>").openPopup();
+                        var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+                        var osmAttrib='Map data © OpenStreetMap contributors';
+                        var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
+                        map.addLayer(osm);
+                    </script>
+                <?php
+                }
+
 $titre = 'Detail de la randonnées';
 $contenu = ob_get_clean();
 
