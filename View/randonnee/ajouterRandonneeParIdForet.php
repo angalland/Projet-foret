f05f03b656ee9e74c44e3ecf0b9e458f4eccca43
angalland <angalland@laposte.net>
2023-08-01 10:12:37 +0200

    modification formulaire addRandonnee

--- View/randonnee/ajouterRandonneeParIdForet.php
+++ View/randonnee/ajouterRandonneeParIdForet.php
@@ -19,52 +19,6 @@ ob_start();?>
     <input type="number" name="difficulte" id="difficulte" class="inputConnexion">
     </div>
 
-    <div id="map" style="width:50%;height:400px;"></div>
-        <script>
-            // initialisation de la carte leaflet et du zoom
-            var map = L.map('map').setView([48.581647, 7.750522], 7);
-
-            // gestion des tuiles
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }).addTo(map);
-
-            // affiche la longitude et la latitude du point sur lequel ou on clique
-            var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-            var osmAttrib='Map data © OpenStreetMap contributors';
-            var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
-            map.addLayer(osm);
-            map.on('click',clicSurCarte);
-            function clicSurCarte(event){
-                var latlong=event.latlng
-                alert("Longitude - Latitude : "+latlong);
-            }
-            </script>
-
-    <div class="divAddRandonnee">
-        <div class="addPointRandonnee">
-            <h4 class="formH3 addH3Foret">Ajouter un point de départ</h4>
-            <label for="point_depart" class="addLabel">Longitude, Lattitude : </label>
-            <input type="text" name="point_depart" id="point_depart" class="inputPointRandonnee" placeholder="Exemple : 48.789855, 7.568951">
-            <input class="buttonPointRandonnee" type="submit" name="submitAddForet" value='Ajouter le point de départ'>
-        </div>
-        
-        <div class="addPointRandonnee">
-            <h4 class="formH3 addH3Foret">Ajouter un point</h4>
-            <label for="point" class="addLabel">Longitude, Lattitude : </label>
-            <input type="text" name="point" id="point" class="inputPointRandonnee" placeholder="Exemple : 48.789855, 7.568951">
-            <input class="buttonPointRandonnee" type="submit" name="submitAddForet" value='Ajouter un point'>
-        </div>
-
-        <div class="addPointRandonnee">
-            <h4 class="formH3 addH3Foret">Ajouter un point d'arrivée</h4>
-            <label for="point_arrivee" class="addLabel">Longitude, Lattitude : </label>
-            <input type="text" name="point_arrivee" id="point_arrivee" class="inputPointRandonnee" placeholder="Exemple : 48.789855, 7.568951">
-            <input class="buttonPointRandonnee" type="submit" name="submitAddForet" value='Ajouter un point'>
-        </div>
-    </div>
-
     <div class="addButtonForet ">
         <input class="buttonPointRandonneeAdd" type="submit" name="submitAddForet" value='Ajouter une randonnée'>
     </div>
