e4755f5618b386f51f39557585cc1a6eef9aff50
angalland <angalland@laposte.net>
2023-08-01 09:11:29 +0200

    ajout leaflet form addForet

--- View/randonnee/ajouterRandonneeParIdForet.php
+++ View/randonnee/ajouterRandonneeParIdForet.php
@@ -2,6 +2,8 @@
 ob_start();?>
 <form action="" method="POST">
 
+    <h3>Ajouter une randonnée</h3>
+
     <label for="nom_randonnee">Nom de la randonnee : </label>
     <input type="text" name="nom_randonnee" id="nom_randonnee">
 
@@ -10,7 +12,31 @@ ob_start();?>
 
     <label for="difficulte">Difficulte : </label>
     <input type="number" name="difficulte" id="difficulte">
-    
+
+
+    <div id="map" style="width:400px;height:400px;margin:30px"></div>
+        <script>
+            // initialisation de la carte leaflet et du zoom
+            var map = L.map('map').setView([48.581647, 7.750522], 7);
+
+            // gestion des tuiles
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+
+            // affiche la longitude et la latitude du point sur lequel ou on clique
+            var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+            var osmAttrib='Map data © OpenStreetMap contributors';
+            var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
+            map.addLayer(osm);
+            map.on('click',clicSurCarte);
+            function clicSurCarte(event){
+                var latlong=event.latlng
+                alert("Longitude - Latitude : "+latlong);
+            }
+            </script>
+</form>
 <?php
 $titre = 'ajouter une randonnée';
 $contenu = ob_get_clean();
