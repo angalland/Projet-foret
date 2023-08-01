3546c9c9c845b5b7d262e495d6a2fde7cf7f6bc8
angalland <angalland@laposte.net>
2023-08-01 13:37:44 +0200

    modification form addPoint

--- View/randonnee/ajouterParcoursParRandonnee.php
+++ View/randonnee/ajouterParcoursParRandonnee.php
@@ -31,8 +31,10 @@ ob_start();?>
     <div class="divAddRandonnee">
         <div class="addPointRandonnee">
             <h4 class="formH3 addH3Foret">Ajouter un point de départ</h4>
-            <label for="point_depart" class="addLabel">Longitude, Lattitude : </label>
-            <input type="text" name="point_depart" id="point_depart" class="inputPointRandonnee" placeholder="Exemple : 48.789855, 7.568951">
+            <label for="point_depart" class="addLabel">Longitude : </label>
+            <input type="number" name="point_depart_longitude" id="point_depart" class="inputPointRandonnee" placeholder="Exemple : 48.789855" step="0.000001">
+            <label for="point_depart" class="addLabel">Lattitude : </label>
+            <input type="number" name="point_depart_lattitude" id="point_depart" class="inputPointRandonnee" placeholder="Exemple : 7.568952" step="0.000001">
             <input class="buttonPointRandonnee" type="submit" name="submitAddParcoursPointDepart" value='Ajouter le point de départ'>
         </div>
         
