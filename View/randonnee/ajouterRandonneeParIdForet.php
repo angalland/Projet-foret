be5721ba027f6cbce5dfa604a1a1a194c4f8e23d
angalland <angalland@laposte.net>
2023-08-01 09:06:03 +0200

    form addRandonnee

--- View/randonnee/ajouterRandonneeParIdForet.php
+++ View/randonnee/ajouterRandonneeParIdForet.php
@@ -1,8 +1,17 @@
 <?php
 ob_start();?>
+<form action="" method="POST">
 
+    <label for="nom_randonnee">Nom de la randonnee : </label>
+    <input type="text" name="nom_randonnee" id="nom_randonnee">
+
+    <label for="duree">Durée en Min : </label>
+    <input type="number" name="duree" id="duree">
+
+    <label for="difficulte">Difficulte : </label>
+    <input type="number" name="difficulte" id="difficulte">
+    
 <?php
-
 $titre = 'ajouter une randonnée';
 $contenu = ob_get_clean();
 
