8781edafeeff92f1dab621a150b86181a032b71a
angalland <angalland@laposte.net>
2023-08-01 08:34:14 +0200

    addrandonnee from choisir foret

--- Controller/AdminRandonneeController.php
+++ Controller/AdminRandonneeController.php
@@ -7,6 +7,12 @@ class AdminRandonneeController {
 
     // affiche la page ajouter une randonnee
     public function viewAddRandonnee(){
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->prepare("
+            SELECT *
+            FROM foret        
+        ");
+        $requete->execute();
         require "view/randonnee/ajouterRandonnee.php";
     }
 }
--- View/randonnee/ajouterRandonnee.php
+++ View/randonnee/ajouterRandonnee.php
@@ -1,6 +1,25 @@
 <?php
 ob_start();?>
+<form id="formAddForet" class="formH3 addH3Foret" action="index.php?action=viewUpdateForetById" method="POST">
+    <h3 class="formH3 addH3Foret">Choississez la forêt dont il faut rajouter une randonnée</h3>
 
+    <div class="addDivForet">
+        <label class="addLabel" for="nom_foret">Forêt : </label>
+        <select name="foret" id="nom_foret" class="inputConnexion">
+            <option value=""></option>
+            <?php
+            foreach ($requete as $foret){?>
+                <option value="<?=$foret["id_foret"]?>">
+                <?=$foret['nom_foret']?></option>                      
+            <?php
+            }?>
+        </select>
+    </div>
+
+    <div class="addButtonForet ">
+        <input class="button updateButtonForet" type="submit" name="submitUpdateForet" value='Modifier'>
+    </div>
+</form>
 <?php
 // affiche un message succes si il y en a un 
 if (isset($_SESSION['messageSucces'])) {?>
