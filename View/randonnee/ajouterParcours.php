c2745f18f51574bcdd608d24593123c56068d4f9
angalland <angalland@laposte.net>
2023-08-03 12:53:05 +0200

    modification titre

--- View/randonnee/ajouterParcours.php
+++ View/randonnee/ajouterParcours.php
@@ -1,7 +1,7 @@
 <?php
 ob_start();?>
 <form id="formAddForet" class="formH3 addH3Foret" action="index.php?action=viewAddParcoursByRandonnee" method="POST">
-    <h3 class="formH3 addH3Foret">Choississez la randonnée dont il faut rajouter un parcours</h3>
+    <h3 class="formH3 addH3Foret">Choississez la randonnée dont il faut rajouter un point</h3>
 
     <div class="addDivForet">
         <label class="addLabel" for="nom_randonnee">Randonnée : </label>
