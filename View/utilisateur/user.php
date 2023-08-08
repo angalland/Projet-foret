10b5de84073166a471b7304c09b9ec2c244143a2
angalland <angalland@laposte.net>
2023-08-08 09:32:20 +0200

    modal email modal mot de passe

--- View/utilisateur/user.php
+++ View/utilisateur/user.php
@@ -27,7 +27,7 @@ ob_start();?>
     <button class="btn-open modal-trigger  buttonUpdateUser">Supprimer compte</button>
 </div>
 
-<!-- Modal caché -->
+<!-- Modal caché email-->
 <div class="modal-container">
     <div class="overlay modal-trigger">
     </div>
@@ -38,6 +38,26 @@ ob_start();?>
 
                 <div>
                     <input class="inputConnexion" type="email" placeholder="email" name="email">
+                </div>
+
+                <div>
+                    <input class="button" type="submit" value='Modifier'>
+                </div>
+            </form>
+        </div>
+</div>
+
+<!-- Modal caché mot de passe-->
+<div class="modal-container">
+    <div class="overlay modal-trigger">
+    </div>
+        <div class="modal">
+            <button class="close-modal modal-trigger">X</button>
+            <form id="formConnexion" method="POST" action="index.php?action=inscription">
+                <h3 class="formH3"> Modifier l'email </h3>
+
+                <div>
+                    <input class="inputConnexion" type="text" placeholder="Mot de passe" name="passe">
                 </div>
 
                 <div>
