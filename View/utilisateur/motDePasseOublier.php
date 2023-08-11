3bd9695f3a517d387a61236a051260098686398b
angalland <angalland@laposte.net>
2023-08-11 08:45:10 +0200

    form mdpOulier

--- View/utilisateur/motDePasseOublier.php
+++ View/utilisateur/motDePasseOublier.php
@@ -1,8 +1,17 @@
 <?php
-ob_start();
-
-?>
-
+ob_start();?>
+<form id="formConnexion" method="POST" action="index.php?action=updatePassword">
+    <h3 class="formH3"> Modifier le mot de passe </h3>
+    <div>
+        <input class="inputConnexion" type="password" placeholder="Mot de passe" name="password">
+    </div>
+    <div>
+        <input class="inputConnexion" type="password" placeholder="Confirmer mot de passe" name="confirmPassword">
+    </div>
+    <div>
+        <input class="button" type="submit" value='Modifier' name="updatePassword">
+    </div>
+</form>
 <?php
 $titre = 'Mot de passe oublier';
 $contenu = ob_get_clean();
