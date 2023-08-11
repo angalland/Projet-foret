ba367189ed8af1a7ca2b2c98f204e7aff1a31894
angalland <angalland@laposte.net>
2023-08-11 08:12:23 +0200

    view connexion mdp oublie

--- View/utilisateur/connexion.php
+++ View/utilisateur/connexion.php
@@ -44,6 +44,33 @@ ob_start();
         </div>
 </div>
 
+<!-- modal caché mot de passe oublier -->
+<div class="modal-container">
+    <div class="overlay modal-trigger">
+    </div>
+        <div class="modal">
+            <button class="close-modal modal-trigger">X</button>
+            <form id="formConnexion" method="POST" action="index.php?action=inscription">
+                <h3 class="formH3"> Inscription </h3>
+                <div>
+                    <input class="inputConnexion" type="text" placeholder="Pseudo" name="pseudo">
+                </div>
+                <div>
+                    <input class="inputConnexion" type="email" placeholder="email" name="email">
+                </div>
+                <div>
+                    <input class="inputConnexion" type="password" placeholder="Mot de passe" name="password">
+                </div>
+                <div>
+                    <input class="inputConnexion" type="password" placeholder="Confirmer mot de passe" name="confirmPassword">
+                </div>
+                <div>
+                    <input class="button" type="submit" value='inscription'>
+                </div>
+            </form>
+        </div>
+</div>
+
 <form id="formConnexion" method="POST" action="index.php?action=login">
     <h3 class="formH3">Connexion</h3>
     <div>
@@ -61,7 +88,7 @@ ob_start();
 </form>
 
 <button class="btn-open modal-trigger button">S'inscrire</button>
-
+<button class="btn-open modal-trigger button">Mot de passe oublier ?</button>
 </div>
 
 <?php
